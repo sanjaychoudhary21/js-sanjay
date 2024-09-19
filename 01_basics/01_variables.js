a8aba45bf1a369a1478984be5c764c6159e81312
Sanjay choudhary <sanjayjhuriya85@gmail.com>
2024-09-19 09:25:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =17228
+let accountEmail = "sanjayjhuriya8@google.com"
+var accountPassword = 14234
+accountCity ="jaipur"
+let accountstate;
+
+
+accountEmail = "esgf@hc.com"
+accountPassword = 7867
+accountCity = "jaipr"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+/*
+prefer not to use var
+
+*/
