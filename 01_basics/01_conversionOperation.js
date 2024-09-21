0587bdb177f81411cd14df93ed1f105127a31490
Sanjay choudhary <sanjayjhuriya85@gmail.com>
2024-09-21 11:56:37 +0000

    Conversion typeof

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "sanjay"
+// null = 0
+// undefined = not a number
+// use string for null = not a number
+// true = 1
+
+//"33" => 33
+// "33abc" => nan
+//true => 1; false => 0
+//console.log(typeof (score));
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // nan = not a number
+let isLoggedIn = "sanjay"
+
+//1 = true 0 = false
+
+//"" = false
+// "sanjay" = true
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
